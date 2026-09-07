8a705139905d742ea1eb7838f5752628d27dfa62
MuhammadMullah <muhammadmullah16@gmail.com>
2026-09-08 00:06:02 +0300

    fix credo issues

--- app/config/config.exs
+++ app/config/config.exs
@@ -73,7 +73,7 @@ config :tailwind,
 # Configure Elixir's Logger
 config :logger, :default_formatter,
   format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  metadata: [:request_id, :entry_uuid, :filename_base, :key, :reason, :error]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
